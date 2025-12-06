24ac0d1e6cf3077124a4fe67dd42cc4ea8eaf18f
Max Lin <sky1381@outlook.com>
2025-12-05 16:11:05 -0800

    Fixed errors, more checks, added in getting alt, vel, and acc

--- guidence/guidence.ino
+++ guidence/guidence.ino
@@ -1,12 +1,13 @@
 #include <Arduino.h> // Don't know if neccesary, a lot of example code includes this.
 #include <Arduino_BMI270_BMM150.h> // IMU
 #include <Arduino_LPS22HB.h> // Barometer
-#include <Vector.h>
+#include <cmath> // Exponentation
 
 const float targetAltitude = 750 / 3.281; // Convert feet target to meters
 
+bool hasLaunched = false; // Recalibrate orientation at launch.
+bool hasMECO = false; // Avoid activating the brakes during launch to avoid odd Apogee calculations.
 bool hasApogee = false; // Switch code after Apogee to target landing time
-bool hasLaunched = false; // Avoid activating the brakes during launch to avoid odd Apogee calculations.
 
 float brakeTarget = 0;
 
@@ -14,32 +15,66 @@ int brakePin = 9; // PWM pin connected to brake servo
 
 float gravity = 9.81; // Value of gravity
 
-int gravAcceleration = 0; // Check that we have negative acceleration for a few frames before declaring hasLaunched.
+int hasLaunchedCheck = 0; // Check that we have postive velocity for a few frames before declaring hasLaunched.
+int hasMECOCheck = 0; // Check that we have negative acceleration for a few frames before declaring hasMECO.
+float groundPressure = 0;//Used to calibrate the barometric altimeter
+
+//Need to figure out if X, Y, or Z is up (use Y for now).
+//For some reason, Vector3s are not supported.
+//REMEMBER, THESE ARE LOCAL VELOCITY AND ACCELERATIONS, we will need to find world velocity and accelerations later.
+float velocityX = 0.0;
+float velocityY = 0.0;
+float velocityZ = 0.0;
+
+float accelerationX = 0.0;
+float accelerationY = 0.0;
+float accelerationZ = 0.0;
+
+float rotationX = 0.0;//Ew Euler angles
+float rotationY = 0.0;
+float rotationZ = 0.0;
 
 float getAltitude() {
-  // Replace with IMU or barometer altitude reading
-  return (float) 0;
+  //Barometric altitude.
+  float pressure = BARO.readPressure();
+  return (float) 44330*(1-pow(pressure / groundPressure, 1/5.255)); // https://physics.stackexchange.com/questions/333475/how-to-calculate-altitude-from-current-temperature-and-pressure
 }
 
-Vector getVelocity() {
+void updateVelocity() {
   // Replace with IMU velocity reading or derivative of altitude.
-  // Need Vector3 with velocity because drag = velocity^2
-  return (Vector) (0, 0, 0);
+  // Need to get different axii because drag = velocity^2, so we can't just calculate one-dimension.
+  velocityX += accelerationX; // Want to try integrating acceleration, might drift, but since we only need it to work for ~5 sec, it should be fine.
+  velocityY += accelerationY;
+  velocityZ += accelerationZ;
 }
 
-float getAcceleration() {
-  // Replace with IMU acceleration reading, derivative of velocity.
-  return (float) 0;
+void updateAcceleration() {
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(accelerationX, accelerationY, accelerationZ);
+  }
+}
+
+void updateOrientation() {
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(rotationX, rotationY, rotationZ);
+  }
 }
 
 void setBrakes(float value) {
   // Value between 0.0 and 1.0 scaled to 0-255 for PWM
+  // Note: May have to use CFD to get drag values for a bunch of different orientations and drag brake deployments, and then use that to calculate how much we need to open the brakese to get the amount of drag we want.
+  // However, the force on the brakes is probaby about equal to the amount of drag we need, so we might be able to just do a small trig. calculation to get the amount of drag we need.
+  // However, the orientation of the rocket will change the strength of the drag brakes.
   int pwm = (int) (value * 255);
   analogWrite(brakePin, pwm);
 }
 
+//Power up system right before launch.
 void setup() {
   BARO.begin();
+  groundPressure = BARO.readPressure(); // Set pressure here to avoid potential interference from the motor.
+
+  IMU.begin();
   pinMode(brakePin, OUTPUT);
 }
 
@@ -52,11 +87,12 @@ float getApogee(float calAltitude, float calVelocity) {
 
 void loop() {
   float altitude = getAltitude();
-  Vector velocity = getVelocity();
-  float acceleration = getAcceleration();
+  updateVelocity();
+  updateAcceleration();
   //float apogee = getApogee();
 
-  if (hasLaunched) {//Want to avoid activating brakes until after launch
+  if (hasLaunched) {
+  if (hasMECO) {//Want to avoid activating brakes until after launch
     if (!hasApogee) {
       if (altitude < targetAltitude) {
         float diff = targetAltitude - altitude;
@@ -70,14 +106,44 @@ void loop() {
       }
     } else {
       brakeTarget = 0; // brakes stay retracted
+
+      BARO.end();
     }
   } else {
-    if (acceleration < 0) {
+    if (accelerationY < 9.75) {// Small value to avoid accidental activation before launch.
+      hasMECOCheck++;
+    } else {
+      hasMECOCheck = 0;
+    }
 
+    if (hasMECOCheck >= 5) {
+      hasMECO = true;
+    }
+    brakeTarget = 0;
+  }
+  } else {
+    if (accelerationY > 0.05) {// Small value to avoid accidental activation before launch.
+      hasLaunchedCheck++;
+    } else {
+      hasLaunchedCheck = 0;
+    }
+
+    if (hasLaunchedCheck >= 5) {
+      hasLaunched = true;
+
+      //Reset velocity values to 0 due to the intergration.
+      velocityX = 0.0;
+      velocityY = 0.0;
+      velocityZ = 0.0;
+
+      //Reset orientation values due to Earth's rotation.
+      rotationX = 0.0;
+      rotationY = 0.0;
+      rotationZ = 0.0;
     }
     brakeTarget = 0;
   }
 
-  setBrakes(brakeTarget);//Avoid setting the brakes multiple times in 1 frame to avoid bugs.
-  delay(50); // small loop delay
+  setBrakes(brakeTarget);//Avoid setting the brakes multiple times in 1 frame to maybe avoid bugs.
+  delay(20); // small loop delay, decreased from 50 to 20 to decrease activation time from 250 ms to 100 ms
 }
